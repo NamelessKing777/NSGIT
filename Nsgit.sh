70f53fa3078701b1a6550c824e853e30b0abd2f7
Near Shelby <74830167+nearshelby-yt@users.noreply.github.com>
2020-11-25 19:11:19 +0000

    Add files via upload

---
+++ Nsgit.sh
@@ -0,0 +1,162 @@
+#by Near Shelby
+cinza="\033[1;30m"
+red="\033[0;31m"
+red_claro="\033[1;31m"
+blue_escuro="\033[0;34m"
+ciano="\033[1;34m"
+amarelo="\033[1;33m"
+green='\033[1;32m'
+
+sleep 2.0
+clear
+printf $red
+echo "============================================="
+printf $green
+echo "          _   _       ____ _ _"
+echo "          | \ | |___ / ___(_) |_"
+echo "          |  \| / __| |  _| | __|"
+echo "          | |\  \__ \ |_| | | |_"
+echo "          |_| \_|___/\____|_|\__|"
+printf $red
+echo "============================================="
+printf $green
+echo "                 VERSION 1.0"
+printf $red
+echo "============================================="
+printf $green
+echo "           CRIADOR : NEAR SHELBY"
+printf $red
+echo "============================================="
+printf $green
+echo "              NSGIT INICIANDO"
+printf $red
+echo "============================================="
+apt-get install util-linux -y
+setterm -foreground green -store
+apt-get install git -y
+clear
+printf $green
+echo "     INSTALANDO PACK PARA DDOS/DoS"
+git clone https://github.com/grafov/hulk
+mv hulk /data/data/com.termux/files/home
+git clone https://github.com/dotfighter/torshammer
+mv torshammer /data/data/com.termux/files/home
+git clone https://github.com/cyweb/hammer
+mv hammer /data/data/com.termux/files/home
+git clone https://github.com/Ha3MrX/DDos-Attack
+mv DDos-Attack /data/data/com.termux/files/home
+git clone https://github.com/XCHADXFAQ77X/XERXES
+mv XERXES /data/data/com.termux/files/home
+clear
+printf $green
+echo "     INSTALANDO PACK DE PHISHING"
+git clone https://github.com/Cesar-Hack-Gray/SocialSploit
+mv SocialSploit /data/data/com.termux/files/home
+git clone https://github.com/htr-tech/nexphisher
+mv nexphisher /data/data/com.termux/files/home
+git clone https://github.com/yezz123/Phisher
+mv Phisher /data/data/com.termux/files/home
+git clone https://github.com/DarkSecDevelopers/HiddenEye-Legacy
+mv HiddenEye-Legacy /data/data/com.termux/files/home
+git clone https://github.com/PSecurity/ps.ngrok
+mv ps.ngrok /data/data/com.termux/files/home
+clear
+printf $green
+echo "   INSTALANDO PACK PARA BRUTE FORCE"
+git clone https://github.com/Gameye98/Black-Hydra
+mv Black-Hydra /data/data/com.termux/files/home
+git clone https://github.com/samsesh/SocialBox-Termux
+mv SocialBox-Termux /data/data/com.termux/files/home
+git clone https://github.com/Gameye98/FBBrute
+mv FBBrute /data/data/com.termux/files/home
+clear
+printf $green
+echo "   INSTALANDO METASPLOIT"
+echo "     ISSO PODE DEMORAR"
+apt-get install unstable-repo -y
+apt-get install metasploit -y
+clear
+printf $green
+echo "   INSTALANDO PACK DE SCRIPTS WEB HACKING"
+git clone https://github.com/nearshelby-yt/ipnsNEAR
+mv ipnsNEAR /data/data/com.termux/files/home
+git clone https://github.com/xXh4x0RXx/C4NET
+mv C4NET /data/data/com.termux/files/home
+git clone https://github.com/shoowl/backdoortst
+mv backdoortst /data/data/com.termux/files/home
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+mv RED_HAWK /data/data/com.termux/files/home
+git clone https://github.com/la-deep-web/Phoneinfoga
+mv Phoneinfoga /data/data/com.termux/files/home
+clear
+printf $green
+echo "INSTALANDO SCRIPTS PARA ANALIZE DE VULNERABILIDADE"
+apt-get install nmap -y
+git clone https://github.com/TechnicalMujeeb/TM-scanner
+mv TM-scanner /data/data/com.termux/files/home
+git clone https://github.com/sullo/nikto
+mv nikto /data/data/com.termux/files/home
+clear
+printf $green
+echo "   INSTALANDO SCRIPTS DE PERSONALIZADORAS"
+git clone https://github.com/xXh4x0RXx/ouija
+mv ouija /data/data/com.termux/files/home
+git clone https://github.com/Bhai4You/Termux-Banner
+mv Termux-Banner /data/data/com.termux/files/home
+clear
+printf $green
+echo "   INSTALANDO SCRIPTS DE SMS SPAM"
+git clone https://github.com/KiritoOfficial/spamrito
+mv spamrito /data/data/com.termux/files/home
+git clone https://github.com/LimerBoy/Impulse
+mv Impulse /data/data/com.termux/files/home
+clear
+printf $green
+echo "   INSTALANDO SCRIPTS DE CRIAR VIRUS"
+git clone https://github.com/LOoLzeC/Evil-create-framework
+mv Evil-create-framework /data/data/com.termux/files/home
+git clone https://github.com/Junior60/vbug
+mv vbug /data/data/com.termux/files/home
+git clone https://github.com/Hacking-pch/papaviruz
+mv papaviruz /data/data/com.termux/files/home
+clear
+printf $green
+echo "   INSTALANDO SCRIPTS UTEIS"
+git clone https://github.com/nearshelby-yt/NSSI
+mv NSSI /data/data/com.termux/files/home
+git clone https://github.com/nearshelby-yt/NSSIV2
+mv NSSIV2 /data/data/com.termux/files/home
+git clone https://github.com/doctor154/NgrokCustomLInk
+mv NgrokCustomLInk /data/data/com.termux/files/home
+clear
+printf $green
+echo "   INSTALANDO SCRIPT DE CRIPTOGRAFAR"
+git clone https://github.com/xXh4x0RXx/4cRypt
+mv 4cRypt /data/data/com.termux/files/home
+clear
+printf $green
+echo "   INSTALANDO COISAS IMPORTANTES"
+apt-get install python -y
+apt-get install python2 -y
+apt-get install php -y
+apt-get install curl -y
+apt-get install wget -y
+clear
+printf $red
+echo "============================================="
+printf $green
+echo "          _   _       ____ _ _"
+echo "          | \ | |___ / ___(_) |_"
+echo "          |  \| / __| |  _| | __|"
+echo "          | |\  \__ \ |_| | | |_"
+echo "          |_| \_|___/\____|_|\__|"
+printf $red
+echo "============================================="
+printf $green
+echo "          OBRIGADO POR USAR O NSGIT" 
+printf $red
+echo "============================================="
+printf $green
+echo "          CODADO PELO : NEAR SHELBY"
+printf $red
+echo "============================================="
